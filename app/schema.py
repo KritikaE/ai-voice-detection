416afb19d270cc730f9569212273430d0ef061e3
KritikaE <kritika.one@gmail.com>
2026-02-05 23:49:00 +0530

    Updating schema 2

--- app/schema.py
+++ app/schema.py
@@ -1,10 +1,17 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 from typing import Optional
 
 class AudioRequest(BaseModel):
-    audio_base64: str
+    audio_base64: str = Field(
+        ...,
+        alias="Audio Base64 Format"
+    )
     language: Optional[str] = None
     audio_format: Optional[str] = None
+
+    class Config:
+        allow_population_by_field_name = True
+        extra = "allow"
 
 
 class PredictionResponse(BaseModel):
