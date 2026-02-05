46ed3aa4a33e352e0f966daa7634a5ca8347c3b5
KritikaE <kritika.one@gmail.com>
2026-02-06 02:44:43 +0530

    fixed schema import

--- app/schema.py
+++ app/schema.py
@@ -1,15 +1,20 @@
 from pydantic import BaseModel, Field
 from typing import Optional
 
+
 class AudioRequest(BaseModel):
     audio_base64: Optional[str] = Field(
         None,
         alias="audioBase64Format"
     )
-
     language: Optional[str] = None
     audio_format: Optional[str] = None
 
     class Config:
         allow_population_by_field_name = True
         extra = "allow"
+
+
+class PredictionResponse(BaseModel):
+    classification: str
+    confidence: float
